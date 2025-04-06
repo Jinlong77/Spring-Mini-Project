8507c8d6bfde4c27d0e19620c95cfd38baa448cf
Jinlong77 <kimlongmoeun@gmail.com>
2025-04-06 21:56:24 +0700

    change directory entity

--- src/main/java/org/kshrd/gamifiedhabittracker/service/AppUserService.java
+++ src/main/java/org/kshrd/gamifiedhabittracker/service/AppUserService.java
@@ -1,6 +1,5 @@
 package org.kshrd.gamifiedhabittracker.service;
 
-import org.kshrd.gamifiedhabittracker.model.dto.AppUserEntity;
 import org.kshrd.gamifiedhabittracker.model.dto.request.AppUserRequest;
 import org.kshrd.gamifiedhabittracker.model.dto.response.AppUserDTO;
 
